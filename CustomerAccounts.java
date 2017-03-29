ee75c12b55bd2919f2a714029940fefb270def68
innocabije <innocabije@gmail.com>
2017-03-28 23:47:27 -0500

    Add files via upload
    CustomerAcc java. let's focus on finishing this one first.

---
+++ CustomerAccounts.java
@@ -0,0 +1,47 @@
+
+public class CustomerAccounts {
+	
+	private String name;
+	/**
+	 * 7 digits tax ID.
+	 */
+	private int taxID;
+	/**
+	 * Complete address, example. 123 North Street, Ames Iowa.
+	 */
+	private String address;
+	
+	public CustomerAccounts(String name, int taxID, String address) {
+		this.name = name;
+		this.taxID = taxID;
+		this.address = address;
+	}
+	
+	public void addCustomerAccounts() {
+		
+	}
+	
+	public void removeCustomerAccount() {
+		
+	}
+	
+	public void editCustomerName() {
+		
+	}
+	
+	public void editCustomerAddress() {
+		
+	}
+	
+	public String getCustomerName()	 {
+		return " "	;
+	}
+	
+	public int getTaxID() {
+		return 0;
+	}
+	
+	public String getAddress() {
+		return " ";
+	}
+}
