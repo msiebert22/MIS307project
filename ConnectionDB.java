f1db894cb4641c7dcc48d27ba16fe6145a4b04ee
innocabije <innocabije@gmail.com>
2017-04-06 20:01:12 -0500

    Update ConnectionDB.java

--- ConnectionDB.java
+++ ConnectionDB.java
@@ -1,9 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
+
 
 
 public class ConnectionDB {
