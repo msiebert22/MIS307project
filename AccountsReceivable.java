45ba756ae1be8aefb02c2fda14abc21e21e5ae0d
innocabije <innocabije@gmail.com>
2017-04-24 19:01:57 -0500

    Update AccountsReceivable.java

--- AccountsReceivable.java
+++ AccountsReceivable.java
@@ -8,51 +8,75 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Scanner;
 public class AccountsReceivable {
-	private static InvoiceDB ii;
+
+	/**
+	 * The main method of the program. Tests every method from InvoiceDB, CustomerAccounts classes.
+	 * @param args
+	 * @throws SQLException
+	 */
 	public static void main(String[] args) throws SQLException {	
-	
+		
+		System.out.println("INVOICE");
+		System.out.println("BEFORE EDIT");
+		//initializes the invoice database
 		createInvoiceDB();
-		
+		//creates an invoicedb
 		InvoiceDB iv = new InvoiceDB(5004);
-		
+		//adds the invoice with taxid amount dates.
 		iv.addInvoice(1004, 10.50, "10/10/10", "11/10/10");
-		iv.amountsReceived(10.50);
-		
+		//Print the database
 		for(int i = 0; i < iv.getInvoiceNumbers().size(); i++) {
 			String checkifPaid = iv.checkifPaid(iv.getInvoiceNumbers().get(i));
-			ii = new InvoiceDB(iv.getInvoiceNumbers().get(i));		
-			System.out.println(ii.toString());
-			System.out.println("Y if paid and N for not yet paid: " + iv.getInvoiceNumbers().get(i) + " " + checkifPaid);
+			iv = new InvoiceDB(iv.getInvoiceNumbers().get(i));		
+			System.out.println(iv.toString());		
+			System.out.println("Yes/No: " + "Paid or Not Paid: InvoiceNumber = " +  iv.getInvoiceNumbers().get(i) + ": " + checkifPaid);
 		}
+		//Edited part.
 		System.out.println();
-		
-		
+		System.out.println("EDITS");
+		//Payment
+		iv.amountsReceived(5004, 10.50);	
+		//Print the contents of the database
 		for(int i = 0; i < iv.getInvoiceNumbers().size(); i++) {
-			
+			String checkifPaid = iv.checkifPaid(iv.getInvoiceNumbers().get(i));
+			iv = new InvoiceDB(iv.getInvoiceNumbers().get(i));	
+		
+			System.out.println(iv.toString());		
+			System.out.println("Yes/No: " + "Paid or Not Paid: InvoiceNumber = " +  iv.getInvoiceNumbers().get(i) + ": " + checkifPaid);
 		}
-	
-		
-	
 
-		
-		
-//		Connection con = ConnectionDB.getConnection();
-//		Statement s = con.createStatement();
-//		ResultSet r = s.executeQuery("SELECT InvoiceNum, TaxID, InvoiceAmount, InvoiceDate, Invoice_DueDate FROM InvoiceDB");
-//		ResultSetMetaData rs = r.getMetaData();
-//		
-//		int col = rs.getColumnCount();
-//		
-//				while(r.next()) {
-//					for(int i = 1; i <=col; i++) {
-//						System.out.println(r.getString(i) + " ");
-//					}
-//						System.out.println();
-//				}
-	
+
+		System.out.println();
+		System.out.println("CUSTOMERDB");
+		//Initializes customer's database
+		createCustomerDB();
+		CustomerAccounts c = new CustomerAccounts(1004);
+		c.addCustomerAccounts("Tobi Wan", "2222 Three Street", "Four", "WA");
+		System.out.println("BEFORE EDIT");
+		//prints the contents of the database
+		for(int i = 0; i < c.getTaxID().size(); i++) {
+			String name = c.getCustomerName(c.getTaxID().get(i));
+			String address = c.getCustomerAddress(c.getTaxID().get(i));
+			int taxID = c.getTaxID().get(i);
+			System.out.println(taxID + " " + name + " " + address);
+		}	
+		System.out.println();
+		System.out.println("EDITS");
+		//Remove a customer
+		c.removeCustomerAccount(1001);
+		c.editAddress(1003, "6666 Never Poor St.", "Seattle", "WA");
+		//Edits a customer
+		c.editCustomerName(1002, "Burning-God");
+		//prints the contents of the database
+		for(int i = 0; i < c.getTaxID().size(); i++) {
+			String name = c.getCustomerName(c.getTaxID().get(i));
+			String address = c.getCustomerAddress(c.getTaxID().get(i));
+			int taxID = c.getTaxID().get(i);			
+			System.out.println(taxID + " " + name + " " + address);
+		}	
 	}
 	/**
-	 * Creates the customer database.
+	 * Creates/initializes the customer database.
 	 * @throws SQLException
 	 */
 	public static void createCustomerDB() throws SQLException {  
@@ -64,7 +88,7 @@ public class AccountsReceivable {
 					} catch (SQLException e) {
 						System.out.println(e.getMessage());
 					}
-					s.execute("CREATE TABLE CustomerDB (TaxID INTEGER PRIMARY KEY, CustomerName VARCHAR(40), CustomerAddressStreet VARCHAR(40), CustomerAddressCity VARCHAR(40), CustomerAddressState VARCHAR(2))");
+					s.execute("CREATE TABLE CustomerDB (TaxID INTEGER, CustomerName VARCHAR(40), CustomerAddressStreet VARCHAR(40), CustomerAddressCity VARCHAR(40), CustomerAddressState VARCHAR(2))");
 					
 					int taxID[] = {1001,1002,1003};
 					String CustomerNames[] = {"Gabe Newell", "Dwyane Wade", "Bill Clinton"};
@@ -78,11 +102,10 @@ public class AccountsReceivable {
 					}
 				}
 			}
-			//System.out.println("CREATED");
 		}
 
 	/**
-	 * Creates the invoice database. 
+	 * Creates/initializes the invoice database. 
 	 * @throws SQLException
 	 */
 	public static void createInvoiceDB() throws SQLException {
@@ -98,8 +121,8 @@ public class AccountsReceivable {
 				int invoiceNum[] = {5001, 5002, 5003};
 				int taxID[] = {1001, 1002,1003};
 				double invoiceAmount[] = {23.56, 112.50, 500.60};
-				String invoiceDates[] = {"09/02/10", "10/01/10", "05/12/10"};
-				String invoice_DueDates[] = {"10/12/10", "11/11/10", "05/25/10"};
+				String invoiceDates[] = {"20/02/08", "10/01/09", "05/12/10"};
+				String invoice_DueDates[] = {"30/12/08", "30/11/10", "05/12/11"};
 				
 				for(int i = 0; i < invoiceNum.length; i++ ){
 					InvoiceDB x = new InvoiceDB(invoiceNum[i]);
@@ -108,7 +131,6 @@ public class AccountsReceivable {
 				
 			}
 		}
-		//System.out.println("CREATED DATABASE FOR INVOICE");
 	}
 
 }
